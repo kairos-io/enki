e2e2eb079c9a5a8be3ae5bb2cef1b2ccc6e65f46
Itxaka <itxaka@kairos.io>
2024-01-25 16:28:11 +0100

    Small fix
    Signed-off-by: Itxaka <itxaka@kairos.io>

--- cmd/genkey.go
+++ cmd/genkey.go
@@ -37,13 +37,13 @@ func NewGenkeyCmd() *cobra.Command {
 				return err
 			}
 
-			for _, t := range []string{"PK", "KEK", "DB"} {
-				l.Infof("Generating %s", t)
-				key := filepath.Join(output, fmt.Sprintf("%s.key", t))
-				pem := filepath.Join(output, fmt.Sprintf("%s.pem", t))
-				der := filepath.Join(output, fmt.Sprintf("%s.der", t))
-				auth := filepath.Join(output, fmt.Sprintf("%s.auth", t))
-				esl := filepath.Join(output, fmt.Sprintf("%s.esl", t))
+			for _, keyType := range []string{"PK", "KEK", "DB"} {
+				l.Infof("Generating %s", keyType)
+				key := filepath.Join(output, fmt.Sprintf("%s.key", keyType))
+				pem := filepath.Join(output, fmt.Sprintf("%s.pem", keyType))
+				der := filepath.Join(output, fmt.Sprintf("%s.der", keyType))
+				auth := filepath.Join(output, fmt.Sprintf("%s.auth", keyType))
+				esl := filepath.Join(output, fmt.Sprintf("%s.esl", keyType))
 				cmd := exec.Command(
 					"openssl",
 					"req", "-nodes", "-x509", "-subj", fmt.Sprintf("/CN=%s/", name),
@@ -52,57 +52,57 @@ func NewGenkeyCmd() *cobra.Command {
 				)
 				out, err := cmd.CombinedOutput()
 				if err != nil {
-					l.Errorf("Error generating %s: %s", t, string(out))
+					l.Errorf("Error generating %s: %s", keyType, string(out))
 					return err
 				}
-				l.Infof("%s generated at %s and %s", t, key, pem)
+				l.Infof("%s generated at %s and %s", keyType, key, pem)
 
-				l.Infof("Converting %s.pem to DER", t)
+				l.Infof("Converting %s.pem to DER", keyType)
 				cmd = exec.Command(
 					"openssl", "x509", "-outform", "DER", "-in", pem, "-out", der,
 				)
 				out, err = cmd.CombinedOutput()
 				if err != nil {
-					l.Errorf("Error generating %s: %s", t, string(out))
+					l.Errorf("Error generating %s: %s", keyType, string(out))
 					return err
 				}
-				l.Infof("%s generated at %s", t, der)
+				l.Infof("%s generated at %s", keyType, der)
 
-				l.Infof("Generating %s.esl", t)
+				l.Infof("Generating %s.esl", keyType)
 				cmd = exec.Command(
 					"sbsiglist", "--owner", uid.String(), "--type", "x509", "--output", esl, der,
 				)
 				out, err = cmd.CombinedOutput()
 				if err != nil {
-					l.Errorf("Error generating %s: %s", t, string(out))
+					l.Errorf("Error generating %s: %s", keyType, string(out))
 					return err
 				}
-				l.Infof("%s generated at %s", t, esl)
+				l.Infof("%s generated at %s", keyType, esl)
 
 				// For PK and KEK we use PK.key and PK.pem to sign it
 				// For DB we use KEK.key and KEK.pem to sign it
 				var signKey string
-				if t == "PK" || t == "KEK" {
+				if keyType == "PK" || keyType == "KEK" {
 					signKey = filepath.Join(output, "PK")
 				} else {
 					signKey = filepath.Join(output, "KEK")
 				}
-				l.Infof("Signing %s with %s", t, signKey)
+				l.Infof("Signing %s with %s", keyType, signKey)
 				cmd = exec.Command(
 					"sbvarsign",
 					"--attr", "NON_VOLATILE,RUNTIME_ACCESS,BOOTSERVICE_ACCESS,TIME_BASED_AUTHENTICATED_WRITE_ACCESS",
 					"--key", fmt.Sprintf("%s.key", signKey),
 					"--cert", fmt.Sprintf("%s.pem", signKey),
 					"--output", auth,
-					fmt.Sprintf("%s", t),
+					fmt.Sprintf("%s", keyType),
 					esl,
 				)
 				out, err = cmd.CombinedOutput()
 				if err != nil {
-					l.Errorf("Error generating %s: %s", t, string(out))
+					l.Errorf("Error generating %s: %s", keyType, string(out))
 					return err
 				}
-				l.Infof("%s generated at %s", t, auth)
+				l.Infof("%s generated at %s", keyType, auth)
 
 			}
 
